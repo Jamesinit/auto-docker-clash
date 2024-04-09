c72073476c948f14e90a0181658e594532374baf
jakevvw <jakevvw@qq.com>
2024-04-10 05:45:45 +0800

    add timing task

---
+++ crontab_add_task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 设置 Python 脚本路径
+dir_path="/home/vv/clash"
+script_name="ParseSubscribe.py"
+
+# 设置 cron 作业的执行时间（在凌晨 6 点执行）
+parse_cron_time="0 6 * * *"
+reload_cron_time="2 6 * * *"
+
+# 将 cron 作业添加到 crontab 文件中
+(crontab -l ; echo "$parse_cron_time /usr/bin/python3  $dir_path/$script_name") |crontab -
+(crontab -l ; echo "$reload_cron_time /usr/bin/zsh  cd $dir_path && docker compose -f docker-compose-clash.yml restart ") |crontab -
